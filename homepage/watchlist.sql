d4ed82883ce1abe90f954c0d2d5b3c054a16299c
kazunited66 <128480694+kazunited66@users.noreply.github.com>
2023-11-29 02:14:18 +1100

    Delete homepage directory

--- homepage/watchlist.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE watchlist(
-    user_id int(11),
-    movie_id int(11),
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id),
-    PRIMARY KEY(user_id, movie_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
