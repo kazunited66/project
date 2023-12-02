116bf68e09be8a5fb5cede73c84e1ee50d7016d1
kazunited66 <128480694+kazunited66@users.noreply.github.com>
2023-12-02 15:21:19 +1100

    Delete AssignmentFinal directory

--- AssignmentFinal/dbConnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "assignment";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
--- AssignmentFinal/main.js
+++
@@ -1,7 +0,0 @@
-
-    $(document).ready(function(){
-
-    $("#div1").load("data.php");
-
-});
-
